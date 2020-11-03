bf452e2530992d4789920a23cef68b3034476841
owpk <vzvz4@mail.ru>
2020-11-03 23:41:57 +0900

    'fix'

--- JavaCore/src/Test/Coller.java
+++ JavaCore/src/Test/Coller.java
@@ -1,39 +1,13 @@
 package Test;
 
-import java.io.*;
 import java.util.ArrayDeque;
-import java.util.Date;
-import java.util.regex.Pattern;
+import java.util.Queue;
 
 public class Coller {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private String name;
-
-    public Coller(String name) {
-        this.name = name;
-=======
     public static void main(String[] args) {
-        int a = 5;
-        System.out.println(rec(5));
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("S");
+        queue.add("O");
+        System.out.println(queue.poll());
     }
-
-    public static int rec(int r) {
-        if (r == 0) return 0;
-        return r + rec(r - 1);
->>>>>>> f038b07a2d68d466d1b619fc9c7cac84c6b6ba43
-=======
-    public static void main(String[] args) throws IOException {
-        File f = new File("tmp");
-        FileWriter writer = new FileWriter(f);
-        Date d = new Date();
-        System.out.println(d.getTime());
-        writer.write(String.valueOf(d.getTime()));
-        writer.close();
-        ArrayDeque
-
-        BufferedReader reader = new BufferedReader(new FileReader("tmp"));
-        System.out.println(reader.readLine());
->>>>>>> feature
-    }
- }
+}
